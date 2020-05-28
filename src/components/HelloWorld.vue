e439d58c111a2d878910f7e0cd556b3a4240fc03
lth <lth13553380334@163.com>
2020-05-28 16:58:53 +0800

    初次提交，已完成商城项目的结构

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="msg">{{msg}}</p>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'hello vue-cli'
+      }
+    }
+  }
+</script>
+<!-- 添加“scoped”属性以将CSS限制到此组件 -->
+<style scoped>
+  .msg {
+    color: red;
+    font-size: 50px;
+  }
+</style>
